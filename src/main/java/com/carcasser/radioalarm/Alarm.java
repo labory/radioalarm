e32c22cda41a0568c61a5bd5ef8de9742975c4bc
labory <labory@mail.ru>
2013-02-04 00:16:13 +0300

    add exit menu item

--- src/main/java/com/carcasser/radioalarm/Alarm.java
+++ src/main/java/com/carcasser/radioalarm/Alarm.java
@@ -3,8 +3,12 @@ package com.carcasser.radioalarm;
 import javax.swing.ImageIcon;
 import javax.swing.UIManager;
 import java.awt.AWTException;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Main class.
@@ -12,10 +16,18 @@ import java.awt.TrayIcon;
 public class Alarm {
 
     public static void main(String[] args) {
+        PopupMenu menu = new PopupMenu();
+        MenuItem exitItem = new MenuItem("Exit");
+        menu.add(exitItem);
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         try {
             SystemTray.getSystemTray().add(
                     new TrayIcon(((ImageIcon) UIManager.getIcon("FileView.computerIcon")).getImage(),
-                            "invisible application"));
+                            "invisible application", menu));
         } catch (AWTException e) {
             e.printStackTrace();
         }
